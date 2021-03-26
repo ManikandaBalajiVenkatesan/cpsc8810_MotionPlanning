260b8798ac253800eb76790dad56b1dd7f833791
ManikandaBalajiVenkatesan <manikav@clemson.edu>
2021-03-26 11:15:27 -0400

    readme

--- 3_A star algorithm/astar.py
+++ 3_A star algorithm/astar.py
@@ -4,9 +4,10 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to Clemson University and the authors.
-# 
+#
 # Author: Ioannis Karamouzas (ioannis@g.clemson.edu)
 #
+import pprint
 
 
 # Compute the optimal path from start to goal.
@@ -20,11 +21,15 @@ forward = [[-1,  0], # 0: go north
 # The car can perform 3 actions: -1: right turn and then move forward, 0: move forward, 1: left turn and then move forward
 action = [-1, 0, 1]
 action_name = ['R', 'F', 'L']
-cost = [1, 1, 1] # corresponding cost values
+cost = [1, 1, 10] # corresponding cost values
+
+# action =forward
+# action_name = ['U', 'L', 'R', 'D']
+# cost = [1, 1, 1, 1] # corresponding cost values
 
 # GRID:
 #     0 = navigable space
-#     1 = unnavigable space 
+#     1 = unnavigable space
 grid = [[1, 1, 1, 0, 0, 0],
         [1, 1, 1, 0, 1, 0],
         [0, 0, 0, 0, 0, 0],
@@ -32,8 +37,9 @@ grid = [[1, 1, 1, 0, 0, 0],
         [1, 1, 1, 0, 1, 1]]
 
 init = (4, 3, 0) # (grid row, grid col, orientation)
-                
+
 goal = (2, 0, 1) # (grid row, grid col, orientation)
+
 
 
 heuristic = [[2, 3, 4, 5, 6, 7], # Manhattan distance
@@ -91,25 +97,71 @@ Two data structures are provided for your open and closed lists:
 # ----------------------------------------
 # modify the code below
 # ----------------------------------------
+def neighbour_orientation(parent_node,node):        #to determine the orientation between parent & child node
+    pose = [node[0] - parent_node[0],node[1] - parent_node[1]]
+    for i in range(len(forward)):
+        if pose == forward[i]:
+            return i
+        else:
+            pass
+
+def find_neighbour(grid,parent_node):            #to identify the neighbouring nodes around parent node
+    neighbours = [(parent_node[0],parent_node[1]-1,0), (parent_node[0]-1, parent_node[1],0), (parent_node[0], parent_node[1] + 1,0),(parent_node[0]+ 1, parent_node[1],0)]      #list of all possible nodes
+    feasible_neighbours = []
+    for node in neighbours:                          #to filter out improbable neighbours
+        x_coordinate=int(node[0])
+        y_coordinate=int(node[1])
+        if (0 <= x_coordinate < 5 and 0 <= y_coordinate < 6):   #to check whether neighbour is beyond the grid limit
+            if (grid[x_coordinate][y_coordinate] == 0):         #to check whether neighbour is not a wall
+                grid_orientation = int(neighbour_orientation(parent_node, node))     #finding orientation between parent & neighbour
+                node = [x_coordinate,y_coordinate,grid_orientation]
+                if move_action(parent_node,node) != 2 and move_action(parent_node,node) != -2: #removing neighbours that are behind parent
+                    feasible_neighbours.append((x_coordinate,y_coordinate,grid_orientation))          #appending feasible childeren after filtering
+    return feasible_neighbours
+
+def find_value(action,current_value,location):    #calculation of cost of particular action from parent to neighbour
+    h = heuristic[location[0]][location[1]]       #heuristic cost for that neighbouring node
+    g = current_value.g + 1                       #path cost for child is path cost for parent + 1
+    f = h + g + cost[action+1]                    #total cost for that neighbouring node
+    return f, g
+
+def move_action(current_node,node):        #to determine the action that needs to be taken to move from parent to neighbour
+    action = node[2] - current_node[2]    #difference between orientation of parent and neighbour
+    if action == 3:                             #change in orientation from north to east is 3 which is left turn
+        return -1
+    elif action == -3:                          #change in orientation from east to north is -3 which is right turn
+        return 1
+    else:
+        return action
+
 def compute_path(grid,start,goal,cost,heuristic):
-   
+
     # Use the OrderedSet for your closed list
     closed_set = OrderedSet()
-    
     # Use thePriorityQueue for the open list
-    open_set = PriorityQueue(order=min, f=lambda v: v.f)      
+    open_set = PriorityQueue(order=min, f=lambda v: v.f)
 
-    # Keep track of the parent of each node. Since the car can take 4 distinct orientations, 
-    # for each orientation we can store a 2D array indicating the grid cells. 
-    # E.g. parent[0][2][3] will denote the parent when the car is at (2,3) facing up    
-    parent = [[[' ' for row in range(len(grid[0]))] for col in range(len(grid))], 
+    # Keep track of the parent of each node. Since the car can take 4 distinct orientations,
+    # for each orientation we can store a 2D array indicating the grid cells.
+    # E.g. parent[0][2][3] will denote the parent when the car is at (2,3) facing up
+
+    parent = [[[' ' for row in range(len(grid[0]))] for col in range(len(grid))],
              [[' ' for row in range(len(grid[0]))] for col in range(len(grid))],
              [[' ' for row in range(len(grid[0]))] for col in range(len(grid))],
              [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]]
 
+    # creating a node cost list similar in structure to parent list with inital value of 100
+    # used to determine whether the parent list needs to be updated for a particular child
+    # if the new cost to reach a child is less than the previous calculated cost
+    # then parent list and node_cost list will be updated with new found parent node and cost
+    node_cost = [[[ 100 for row in range(len(grid[0]))] for col in range(len(grid))],
+             [[ 100 for row in range(len(grid[0]))] for col in range(len(grid))],
+             [[ 100 for row in range(len(grid[0]))] for col in range(len(grid))],
+             [[ 100 for row in range(len(grid[0]))] for col in range(len(grid))]]
+
     # The path of the car
     path =[['-' for row in range(len(grid[0]))] for col in range(len(grid))]
-    
+
     x = start[0]
     y = start[1]
     theta = start[2]
@@ -119,20 +171,50 @@ def compute_path(grid,start,goal,cost,heuristic):
     open_set.put(start, Value(f=f,g=g))
 
     # your code: implement A*
+    while open_set: #code runs in loop as long as there is a node in open_set
+        current_node,current_value = open_set.pop()     #current node is minimum from open_set
+
+        if current_node == goal:                        #to stop the code once goal is reached
+            closed_set.add(current_node)                #adding goal to closed_set
+            while current_node != start:                #to back track, from end to start, loop runs till we backtrack to start node
+                parent_node = parent[current_node[2]][current_node[0]][current_node[1]]     #accessing parent list to find the parent node of current node
+                action = move_action(parent_node,current_node)       #to determine what action was required to move from parent node to current node
+                path[parent_node[0]][parent_node[1]] = action_name[action+1]    #updating path list with action required to move from parent to child node
+                current_node = parent_node              #now parent node becomes current node before path identification code runs again
+            pprint.pprint(path)
+            return path,closed_set
+
+        closed_set.add(current_node)        #adding current node to closed_set
+
+        neighbours = find_neighbour(grid, current_node)    #to determine the list of feasible children for current node
+        for neighbour in neighbours:                          #running through all feasible childs
+            actions = move_action(current_node,neighbour)    #determining action required to move from current node to child node
+            total_cost_neighbour, step_cost_neighbour = find_value(actions,current_value,neighbour)  #calculating total & path cost for moving to child node
+            if neighbour not in open_set or closed_set:     # if child is not present in open or closed set then add to open_set
+                open_set.put(neighbour,Value(f=total_cost_neighbour,g=step_cost_neighbour))
+            # incase child is present in open set then new & old value are compared and if new value is low then it is updated
+            elif neighbour in open_set:
+                f_prev = open_set.get(neighbour)    #accessing old value of child from open_set
+                f_prev_f = f_prev.f
+                if total_cost_neighbour < f_prev_f:          #comparing old and new values
+                    open_set.put(neighbour,Value(f=total_cost_neighbour, g=step_cost_neighbour)) #updating open set if new value is less than old value
+
+            #if cost to travel to child is found to be lesser than previously calculated cost for the child then its parent and cost is updated
+            if node_cost[neighbour[2]][neighbour[0]][neighbour[1]] > total_cost_neighbour:
+                parent[neighbour[2]][neighbour[0]][neighbour[1]] = current_node[0],current_node[1],current_node[2]      #updating parent node
+                node_cost[neighbour[2]][neighbour[0]][neighbour[1]] = total_cost_neighbour                                           #updating cost
 
     # Initially you may want to ignore theta, that is, plan in 2D.
     # To do so, set actions=forward, cost = [1, 1, 1, 1], and action_name = ['U', 'L', 'R', 'D']
     # Similarly, set parent=[[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
-
     return path, closed_set
-
 
 if __name__ == "__main__":
     path,closed=compute_path(grid, init, goal, cost, heuristic)
-
+    """
     for i in range(len(path)):
         print(path[i])
-
+    """
     print("\nExpanded Nodes")    
     for node in closed:
         print(node)
@@ -151,7 +233,7 @@ In this case, the elements in your closed set (i.e. the expanded nodes) are:
 (3, 3, 0)
 (2, 3, 0)
 (2, 4, 3)
-(1, 3, 0)
+(1, 3, 0)      
 (2, 5, 3)
 (0, 3, 0)
 (0, 4, 3)
---
+++ 3_A star algorithm/readme.md.txt
@@ -0,0 +1,54 @@
+CPSC 8810 Assignment 3
+
+Done by
+Ajinkya Joglekar		C72342264
+Manikand Balaji Venkatesan	C13093637
+
+Methods created
+
+neighbour_orientation
+	To determine the orientation of child node with respect to parent node
+	Takes parent node and child node as input.
+	Returns orientation as output.
+
+	Finds the differene between child node and parent node coordinates is calculated and compared with forward list that was provided.
+
+find_neighbour
+	To determine the feasible neighbour around a node
+	Takes the grid and current node as input
+	Returns a list of feasible nodes 
+
+	Finds all possible nodes around a node which are node below, left, right and above the current node. Once this list is arrived they are filtered to eliminate nodes that are out of bounds or a non navigable node or a node that is right behind the current node since moving forward, turning left & moving forward , turning right & moving forward are the only possible 3 actions that can be performed by the car. The list of feasible nodes are then returned as output.
+
+find_value
+	To determine the cost of moving to a child node
+	Takes current node's cost , location of child node and the action to be taken as input.
+	Returns the cost of moving to a particular node as output
+	
+	The cost of moving to a node is sum of the heuristic cost of that node, path cost for that node and action cost for the action that is taken to move to that node. The heuristic cost is found from the heuristic cost list, the path cost of child node is path cost of parent plus 1, action cost is found using cost list.
+
+
+move_action
+	To determine the action that needs to be executed by the parent in order to reach the child.
+	Takes current node and child node as input
+	Returns the action to be taken as output
+
+	Action is difference in orientation between child node and parent node. if the action is 1 then it is left turn, if it is -1 then it is right turn. There will be exception where the car would turn from north to east when the action value will be 3 that essentially means left turn similarly for east to north orientation as well.
+
+
+
+Variables
+
+node_cost - a 3d list similar to parent list. This is used to store the cost for each node. The purpose of this is to use this as a condition when updating the parent of any node. A child can have two possible parent nodes therefore it is essential to identify which should be the right parent node. Cost of moving to a child from parent is the right measure with which we can determine the parent list needs to be updated or not. The node_cost elements are initialized to high value like 100 so that as when we identify a child node its value will be less than this place holder value and therefore would get substituted but when the same child node comes again the new cost will be compared against the previous cost and the parent node which would cause the least cost will be choosen.
+
+orientation - the orientation of child with respect to parent.
+neighbours - all possible child nodes of a parent node
+feasible_neighbours - filtered list of neighbors
+action - action that needs to be taken by car to move from parent node to child node.
+neighbour - child node part of neighbours list
+current_value - cost object of current node
+f_prev - previouscost object of node if it is found to be present in open set
+f_prev_f - the previous f value of node if it is found to be present in open_set.
+total_cost_neighbour - the total cost of moving from parent node to a child node.
+
+
